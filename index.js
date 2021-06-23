c96565e464e0c1d04099969e7012599079ed55b3
phamngocthai <ngocthaipham99@gmail.com>
2021-06-23 21:20:00 +0700

    push

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {cors: { origin: "*"}});
+
+app.set('view engine', 'ejs');
+
+app.get('/home', (req, res) =>{
+    res.render('home');
+});
+
+io.on("connection", (socket) => {
+    console.log('User connected :' + socket.id);
+
+    socket.on("message", (data) =>{
+        socket.broadcast.emit('message', data)
+    })
+})
+
+server.listen(3002, () => {
+    console.log('Server running at 3002 port')
+})
+
+
